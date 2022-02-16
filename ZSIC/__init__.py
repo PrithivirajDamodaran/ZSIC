529451ef368d6c11bcb78d44d9bf9ece6f3487c2
Your Name <you@example.com>
2022-02-16 20:18:46 +0530

    final

--- ZSIC/__init__.py
+++ ZSIC/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-from zsic. ZeroShotImageClassification import  ZeroShotImageClassification
-=======
-from zsic.classifier import ZeroShotImageClassification
->>>>>>> 786ea52e309c6858ee442cc19cd2285c7c709fe0
+from zsic.ZeroShotImageClassification import  ZeroShotImageClassification
