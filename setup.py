b659e44c7b2736e62d151e4bbdb2f7726c4b655a
Your Name <you@example.com>
2022-02-16 11:01:14 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-import os
+import setuptools
 
-import pkg_resources
-from setuptools import setup, find_packages
-
-setup(
+setuptools.setup(
     name="ZSIC",
     version="1.0",
-    description="Zeroshot Image Classifier",
     author="Donkey Stereotype",
-    packages=find_packages(),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
+    author_email="",
+    description="Zeroshot image classification",
+    long_description="Zero Shot Image Classification equivalent for HuggingFace Zero Shot Text Classification - By Prithivi Da",
+    url="https://github.com/PrithivirajDamodaran/ZSIC.git",
+    packages=setuptools.find_packages(),
+    install_requires=['ftfy' ,'regex', 'tqdm', 'torch',' torchvision', 'git+https://github.com/openai/CLIP.git']
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+        "Operating System :: OS Independent",
     ],
-    include_package_data=True
 )
