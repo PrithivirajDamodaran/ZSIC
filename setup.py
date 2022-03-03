01d03ee067e851d54528db1bafdacb74256229c9
Prithivida <d.prithiviraj@gmail.com>
2022-03-03 22:08:21 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 import setuptools
 import os
 
-requirements = []
-    try:
-        with open('requirements.txt') as f:
-            requirements = f.read().splitlines()
+os.system("pip install git+https://github.com/openai/CLIP.git") 
+os.system("pip install sentence-transformers")
 
 
 setuptools.setup(
     name="ZSIC",
     version="1.0",
-    author="Donkey Stereotype",
+    author="PithiviDa @ Donkey Stereotype",
     author_email="",
-    description="Zeroshot image classification",
+    description="Zeroshot Image Classification",
     long_description="Zero Shot Image Classification equivalent for HuggingFace Zero Shot Text Classification - By Prithivi Da",
     url="https://github.com/PrithivirajDamodaran/ZSIC.git",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: MIT",
